bb7a78383b4190108fbae0fed7699055aca473c0
MoisesCorcho <55525094+MoisesCorcho@users.noreply.github.com>
2024-03-09 12:33:36 -0500

    Se intercepta error en assertJsonApiError dentro de JsonApiTestResponse
    La especificacion JSON:API dice 'Error objects MUST be returned as an array keyed by errors in the top level of a JSON:API document' con lo cual, cuando no se genere la respuesta de errores con una llave errors que contenga arreglos de objetos, se generará un error predefinido por Laravel que no da muchas pistas sobre el error, entonces, se intercepta dicho error para especificar que ha sucedido.

--- app/JsonApi/JsonApiTestResponse.php
+++ app/JsonApi/JsonApiTestResponse.php
@@ -6,7 +6,7 @@ use Closure;
 use Illuminate\Support\Str;
 use Illuminate\Testing\TestResponse;
 use PHPUnit\Framework\Assert as PHPUnit;
-use PHPUnit\Framework\InvalidArgumentException;
+use PHPUnit\Framework\ExpectationFailedException;
 
 /**
  * Clase llamada a traves del metodo mixin de TestResponse
@@ -34,11 +34,20 @@ class JsonApiTestResponse
         return function($title = null, $detail = null, $status = null) {
             /** @var TestResponse $this */
 
-            $this->assertJsonStructure([
-                'errors' => [
-                    '*' => ['title', 'detail']
-                ]
-            ]);
+            try {
+
+                $this->assertJsonStructure([
+                    'errors' => [
+                        '*' => ['title', 'detail']
+                    ]
+                ]);
+            } catch (\TypeError $th) {
+                PHPUnit::fail(
+                    "Error objects MUST be returned as an array keyed by errors in the top level of a JSON:API document."
+                    .PHP_EOL.PHP_EOL.
+                    $th->getMessage()
+                );
+            }
 
             $title  && $this->assertJsonFragment(['title' => $title]);
             $detail && $this->assertJsonFragment(['detail' => $detail]);
